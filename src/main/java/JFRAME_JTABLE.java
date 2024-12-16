f31855953563d88447b510babc1b8e5fd829dd99
unknown <afrose0.gmail.com>
2024-12-16 14:30:04 +0600

    initial commit

---
+++ src/main/java/JFRAME_JTABLE.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author Omar
+ */
+class JFRAME_JTABLE {
+
+    static void AddRowToTable(Object[] object) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+    
+}
